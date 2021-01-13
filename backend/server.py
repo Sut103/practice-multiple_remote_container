d34ebba35d44bf7360d14e5d60c988cab937b929
Sut103 <kou.103.kan@gmail.com>
2021-01-13 16:06:16 +0000

    サンプルAPI追加

---
+++ backend/server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import make_response, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/", methods=['GET'])
+def index():
+    response = {
+        "message": "Hello from Flask Container"
+    }
+
+    return make_response(jsonify(response))
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='127.0.0.1', port=5000)
